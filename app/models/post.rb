becc812bc190b48125b89d3a14ba543c40110851
dannyb21892 <dannyb21892@verizon.net>
2018-03-19 14:39:53 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    out = true
+    if record.title == nil
+      record.errors[:title] << "Title must exist"
+    else
+      clickbait = ["Won't Believe", "Secret", "Top","Guess"]
+      clickbait.each do |c|
+        if record.title.include?(c)
+          out = false
+        end
+      end
+      record.errors[:title] << 'Not clickbaity enough!' if out
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with MyValidator
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
