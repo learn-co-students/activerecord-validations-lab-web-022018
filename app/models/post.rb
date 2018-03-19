bb9ba6b74ae4f235b12c20bbf81e79411778ecd3
github username <github email address>
2018-03-19 16:58:40 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,30 @@
+class MyValidator < ActiveModel::Validator
+  def validate(post)
+    clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+    valid = ""
+    if post.title
+      clickbait.each do |word|
+        if post.title.include?(word)
+          break
+        else
+          valid = false
+        end
+      end
+    end
+    if valid == false
+      post.errors[:title] << "Not Clickbait"
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+
+  includes ActiveModel::Validator
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates_with MyValidator
+
 end
