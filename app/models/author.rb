77624b96eed920477121bd2bac9a38f90fdaa9ea
Chase <chaseit12@gmail.com>
2018-03-19 18:58:23 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ActiveRecord::Base
+  
+  
+  
+  
+  validates :name, presence: true # THAT IT EXISTS
+  validates :name, uniqueness: true # THAT ITS UNIQUE
+  validates :phone_number, length: {is: 10} 
+ 
 end
