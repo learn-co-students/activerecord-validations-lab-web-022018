f778a4b7c6b9bf16c5eab279b7e2c0d59236c782
Ray Blasko <mray6288@gmail.com>
2018-03-19 16:38:06 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
-class Post < ActiveRecord::Base
+class MyValidator < ActiveModel::Validator
+	def validate(record)
+		unless !record.title || record.title.include?("Won't Believe") || record.title.include?("Top") || record.title.include?("Guess") || record.title.include?("Secret")
+			record.errors[:name] << "Title isn't good enough"
+		end
+	end
 end
+
+class Post < ActiveRecord::Base
+	
+	
+	validates :title, presence: true
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+	include ActiveModel::Validations
+	validates_with MyValidator
+end
+
+
